218f51f9d5a90ef04f501f3057fb61c1148eefe7
Christoph Schweppe <info@cschweppe.de>
2020-05-03 19:55:52 +0200

    Test BreadManagerController a tad more

--- tests/Feature/BreadManagerTest.php
+++ tests/Feature/BreadManagerTest.php
@@ -31,4 +31,28 @@ class BreadManagerTest extends TestCase
         $this->get(route('voyager.bread.edit', 'users'))
              ->assertStatus(200);
     }
+
+    public function test_creating_bread_for_not_existing_table_fails()
+    {
+        $this->get(route('voyager.bread.create', 'not_existing'))
+             ->assertSee('TableNotFoundException');
+    }
+
+    public function test_editing_not_existing_bread_redirects()
+    {
+        $this->get(route('voyager.bread.edit', 'migrations'))
+             ->assertRedirect(route('voyager.bread.create', 'migrations'));
+    }
+
+    public function test_creating_existing_bread_redirects()
+    {
+        $this->get(route('voyager.bread.create', 'users'))
+             ->assertRedirect(route('voyager.bread.edit', 'users'));
+    }
+
+    public function test_create_migrations_bread()
+    {
+        $this->get(route('voyager.bread.create', 'migrations'))
+             ->assertStatus(200);
+    }
 }
