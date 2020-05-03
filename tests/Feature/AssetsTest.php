a8db733767d9b2cfd843d39ce463b96b3cece830
Christoph Schweppe <info@cschweppe.de>
2020-05-03 19:46:39 +0200

    Dont test assets with Dusk #2

---
+++ tests/Feature/AssetsTest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Voyager\Admin\Tests\Feature;
+
+use Illuminate\Support\Facades\Auth;
+use Voyager\Admin\Facades\Voyager;
+use Voyager\Admin\Tests\Unit\TestCase;
+
+class AssetsTest extends TestCase
+{
+    public function test_assets_css_loaded()
+    {
+        $this->get(Voyager::assetUrl('css/voyager.css'))
+             ->assertStatus(200);
+        $this->get(Voyager::assetUrl('css/colors.css'))
+             ->assertStatus(200);
+    }
+
+    public function test_assets_js_loaded()
+    {
+        $this->get(Voyager::assetUrl('js/voyager.js'))
+             ->assertStatus(200);
+    }
+
+    public function test_assets_not_existing_not_loaded()
+    {
+        $this->get(Voyager::assetUrl('some/wrong.file'))
+             ->assertStatus(404);
+    }
+
+    public function test_assets_outside_file_not_loaded()
+    {
+        $this->get(Voyager::assetUrl('../../../../../../.env'))
+             ->assertStatus(404);
+    }
+}
