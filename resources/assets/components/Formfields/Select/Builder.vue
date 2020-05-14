bb161672d1f5b37cc6d4c8c4be10c23b81c2952c
Christoph Schweppe <info@cschweppe.de>
2020-05-14 18:10:54 +0200

    Iprove/Fix select-formfield options

--- resources/assets/components/Formfields/Select/Builder.vue
+++ resources/assets/components/Formfields/Select/Builder.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div v-if="show == 'list-options'">
+        <div v-if="show == 'list-options' || show == 'view-options'">
             <label for="multiple" class="label">{{ __('voyager::generic.multiple') }}</label>
             <input type="checkbox" id="multiple" class="voyager-input" v-model="options.multiple">
 
@@ -26,62 +26,18 @@
                     <tbody>
                         <tr v-for="(option, i) in options.options" :key="'option-'+i">
                             <td>
-                                <input type="text" class="voyager-input w-full" v-model="option.key" placeholder="Key">
+                                <input type="text" class="voyager-input w-full" v-model="option.key" :placeholder="__('voyager::generic.key')">
                             </td>
                             <td>
                                 <language-input
                                     class="voyager-input w-full"
-                                    type="text" placeholder="Value"
+                                    type="text"
+                                    :placeholder="__('voyager::generic.value')"
                                     v-bind:value="option.value"
                                     v-on:input="option.value = $event" />
                             </td>
                             <td>
-                                <button class="button red small icon-only" @click.stop="removeOption(key)">
-                                    <icon icon="trash" />
-                                </button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-        <div v-else-if="show == 'view-options'">
-            <label for="multiple" class="label">{{ __('voyager::generic.multiple') }}</label>
-            <input type="checkbox" id="multiple" class="voyager-input" v-model="options.multiple">
-
-            <div class="w-full flex">
-                <div class="w-4/6">
-                    <h5>{{ __('voyager::generic.options') }}</h5>
-                </div>
-                <div class="w-2/6 text-right">
-                    <button class="button green small icon-only" @click.stop="addOption">
-                        <icon icon="plus" />
-                    </button>
-                </div>
-            </div>
-            <div class="voyager-table">
-                <table>
-                    <thead>
-                        <tr>
-                            <th>{{ __('voyager::generic.key') }}</th>
-                            <th>{{ __('voyager::generic.value') }}</th>
-                            <th>{{ __('voyager::generic.actions') }}</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr v-for="(option, i) in options.options" :key="'option-'+i">
-                            <td>
-                                <input type="text" class="voyager-input w-full" v-model="option.key" placeholder="Key">
-                            </td>
-                            <td>
-                                <language-input
-                                    class="voyager-input w-full"
-                                    type="text" placeholder="Value"
-                                    v-bind:value="option.value"
-                                    v-on:input="option.value = $event" />
-                            </td>
-                            <td>
-                                <button class="button red small icon-only" @click.stop="removeOption(key)">
+                                <button class="button red small icon-only" @click.stop="removeOption(i)">
                                     <icon icon="trash" />
                                 </button>
                             </td>
