87edeef52b505f32f9e675ea2fd1b407b6f6dee7
Christoph Schweppe <info@cschweppe.de>
2020-05-14 17:57:50 +0200

    Finalize select formfield

--- resources/assets/components/Formfields/Select/Read.vue
+++ resources/assets/components/Formfields/Select/Read.vue
@@ -1,11 +1,36 @@
 <template>
     <div>
-        ...
+        <badge v-for="(val, i) in selected" :key="i">{{ val }}</badge>
     </div>
 </template>
 
 <script>
 export default {
     props: ['options', 'data', 'translatable'],
+    methods: {
+        getOptionByKey: function (key) {
+            return this.options.options.filter(function (option) {
+                return option.key == key;
+            })[0] ?? null;
+        }
+    },
+    computed: {
+        selected: function () {
+            var vm = this;
+
+            var data = vm.data ?? [];
+            if (!vm.isArray(data)) {
+                data = [data];
+            }
+
+            return data.map(function (key) {
+                var val = vm.getOptionByKey(key);
+                if (vm.isObject(val)) {
+                    return vm.translate(val.value, true);
+                }
+                return '';
+            });
+        },
+    }
 };
 </script>
