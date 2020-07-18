61b47fa352d75b3cf7a19df686f71b029a5ee85e
Christoph Schweppe <info@cschweppe.de>
2020-07-18 20:36:00 +0200

    Add docs plugin

--- resources/assets/components/Bread/EditAdd.vue
+++ resources/assets/components/Bread/EditAdd.vue
@@ -68,7 +68,6 @@ export default {
     },
     methods: {
         data: function (formfield, value = null) {
-            // TODO: if column.type == relationship, this won't work
             if (formfield.translatable || false && this.output[formfield.column.column] && this.isString(this.output[formfield.column.column])) {
                 Vue.set(this.output, formfield.column.column, this.get_translatable_object(this.output[formfield.column.column]));
             }
@@ -98,7 +97,6 @@ export default {
             return this.input[formfield.column.column];
         },
         getErrors: function (column) {
-            // TODO: if column.type == relationship, this won't work
             return this.errors[column.column] || [];
         },
         save: function () {
