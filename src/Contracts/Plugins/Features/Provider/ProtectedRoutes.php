dd0457d22bcf8530761e74a54eaf8657ddb93a3a
Christoph Schweppe <info@cschweppe.de>
2020-08-13 18:42:56 +0200

    Remove unnecessary uses

--- src/Contracts/Plugins/Features/Provider/ProtectedRoutes.php
+++ src/Contracts/Plugins/Features/Provider/ProtectedRoutes.php
@@ -2,8 +2,6 @@
 
 namespace Voyager\Admin\Contracts\Plugins\Features\Provider;
 
-use Voyager\Admin\Manager\Menu;
-
 interface ProtectedRoutes
 {
     public function provideProtectedRoutes(): void;
--- src/Contracts/Plugins/Features/Provider/PublicRoutes.php
+++ src/Contracts/Plugins/Features/Provider/PublicRoutes.php
@@ -2,8 +2,6 @@
 
 namespace Voyager\Admin\Contracts\Plugins\Features\Provider;
 
-use Voyager\Admin\Manager\Menu;
-
 interface PublicRoutes
 {
     public function providePublicRoutes(): void;
