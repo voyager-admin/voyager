1044bb0530eb29a32c1ffa644eb9a5288f5897b3
Christoph Schweppe <info@cschweppe.de>
2021-04-06 06:58:28 +0200

    Use tailwind again
    As JIT is now implemented in core

--- resources/assets/components/UI/MediaManager.vue
+++ resources/assets/components/UI/MediaManager.vue
@@ -78,7 +78,7 @@
                                     <div class="w-full flex justify-center">
                                         <img :src="file.preview" :class="`rounded object-contain h-${thumbnailSizes[thumbSize]} max-w-full`" v-if="file.preview" />
                                         <img :src="file.file.url" :class="`rounded object-contain h-${thumbnailSizes[thumbSize]} max-w-full`" v-else-if="matchMime(file.file.type, 'image/*')" />
-                                        <div v-else class="`w-full flex justify-center h-${thumbSize}`">
+                                        <div v-else :class="`w-full flex justify-center h-${thumbSize}`">
                                             <icon :icon="getFileIcon(file.file.type)" :size="thumbnailSizes[thumbSize]"></icon>
                                         </div>
                                     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
+    mode: 'jit',
     purge: {
         preserveHtmlElements: true,
         content: [
