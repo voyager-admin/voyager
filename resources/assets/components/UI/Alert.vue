8a2fdf41d0addd70e1977ac29719c4c2934ba7de
Christoph Schweppe <info@cschweppe.de>
2021-05-25 16:39:06 +0200

    Fix alert icon

--- resources/assets/components/UI/Alert.vue
+++ resources/assets/components/UI/Alert.vue
@@ -5,7 +5,7 @@
         uses="border-red-500 border-orange-500 border-yellow-500 border-green-500 border-teal-500 border-blue-500 border-indigo-500 border-purple-500 border-pink-500 border-gray-500"
     >
         <div class="flex space-x-3 items-center">
-            <div class="flex-shrink-0" v-if="icon">
+            <div class="flex-shrink-0 h-5 w-5" v-if="icon">
                 <icon
                     :icon="icon"
                     :size="5"
